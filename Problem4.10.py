1dbe02624b5c6f4081232b72b370227610e76c7e
kushi.mittal@gmail.com <kushi.mittal@gmail.com>
2017-02-14 16:47:27 -0600

    Fixed pca for each image category

--- Problem4.10.py
+++ Problem4.10.py
@@ -2,7 +2,7 @@ import cPickle
 import PIL
 import scipy.misc
 import numpy
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, IncrementalPCA
 
 
 # Converts the raw row vectors to 32 X 32 image arrays
@@ -21,32 +21,14 @@ def row_to_img(row_vector):
 	
 	return my_img
 
-def row_to_img_32_cross_96(row_vector):
-	my_img = numpy.array([numpy.array([None for y in range(96)]) for x in range(32)])
-
-	for row in range(32):
-		for col in range(96):
-
-			col_in_img = col % 32
-
-			if col < 32:
-				my_img[row][col] = row_vector[row*32 + col_in_img]
-			elif col < 64:
-				my_img[row][col] = row_vector[row*32 + col_in_img + 1024]
-			else:
-				my_img[row][col] = row_vector[row*32 + col_in_img + 2048]
-	return my_img
-
-
 def write_img_to_file(img, filename):
-	pass
-	#scipy.misc.imsave(filename, img)
+	scipy.misc.imsave(filename, img)
 
 def get_class_names():
 	class_names = [line.rstrip('\n') for line in open('dataset/names.txt')]
-
 	return class_names
 
+# DONT NEED THIS ANYMORE
 def compute_mean_imgs():
 	# Files containing training data
 	train_files = ["dataset/data_batch_1", "dataset/data_batch_2", "dataset/data_batch_3", "dataset/data_batch_4", "dataset/data_batch_5"]
@@ -82,20 +64,64 @@ def compute_mean_imgs():
 		# Close file
 		f.close()
 
+	print class_count
 	return mean_images
 
+def get_principal_components():
+	# Files containing training data
+	train_files = ["dataset/data_batch_1", "dataset/data_batch_2", "dataset/data_batch_3", "dataset/data_batch_4", "dataset/data_batch_5"]
+
+	images = numpy.array([numpy.array([0 for y in range(3072)]) for x in range(5000)])
+
+	pca_components = numpy.array([None for x in range(10)])
+
+	# Do PCA separately for each category
+	for label in range(10):
+
+		# Store the number of images in each class
+		count = 0
+
+		for curr_file in train_files:
+			# Open the current file
+			f = open(curr_file)
+
+			# Unpack the dataset
+			my_dict = cPickle.load(f)
+
+			# Separate data and labels
+			train_data = my_dict['data']
+			train_labels = my_dict['labels']
+
+			for i in range(len(train_labels)):
+				# Matches the current label
+				if label == (train_labels[i]) and (count < 5000):
+					images[count] = train_data[i]
+
+					count = count + 1
+
+
+			f.close()
+
+		# Memory Efficient PCA
+		ipca = IncrementalPCA(n_components = 20, batch_size = 200)
+		x_ipca = ipca.fit(images)
+		pca_components[label] = x_ipca
+
+	return pca_components
+
+def get test_error(computed, actual):
+	pass
+
 def main():
-	means = compute_mean_imgs()
 	class_names = get_class_names()
 
-	for img_vectors in range(len(means)):
-		img = row_to_img_32_cross_96(means[img_vectors])
+	pca_components = get_principal_components()
 
-		write_img_to_file(img, class_names[img_vectors] + ".png")
-		print img.shape
-		pca = PCA(n_components = 20)
-		pca.fit(img)
-		print(pca.explained_variance_ratio_)
-		break
+	for i in range(10):
+		img = row_to_img(pca_components[i].mean_)
+		write_img_to_file(img, class_names[i] + ".png")
+
+	# compute error on test images using pCA images
+
 
 main()
